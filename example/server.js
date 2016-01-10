c3f169b4bbd2be2929b34c69414e6943e846ba10
adrianburcin <adrian.burcin@yahoo.com>
2016-01-10 20:30:16 +0200

    Draft imdb-parser node module with example of using. For now it fetching only a list of movies from imdb and parse the html

---
+++ example/server.js
@@ -0,0 +1,39 @@
+/**
+ * Created by adrianb on 09/01/16.
+ */
+var express = require('express');
+var app = express(); //Create the Express app
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var imdbParser = require('../lib/imdb-parser');
+
+app.use(cors());
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+var router = express.Router();
+
+//@RequestParam('title')
+router.route('/find').get(function (req, res) {
+    var title = req.param('title');
+    console.log(title);
+
+    imdbParser.find(title)
+              .then((result) => {
+                    res.json(result);
+              })
+              .catch((result) => {
+                    res.json(result);
+              });
+
+});
+
+app.use('/api', router); //This is our route middleware
+
+app.set('port', process.env.PORT || 8000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+});
