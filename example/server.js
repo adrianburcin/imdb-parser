d382a0bb099763305bde092963d2b63961d67f39
adrianburcin <adrian.burcin@yahoo.com>
2016-01-11 22:17:13 +0200

    Refactor imdb-parser Code formatting Improvement way to search for a query, function by a search (type) and return a number of items as you ask for

--- example/server.js
+++ example/server.js
@@ -15,18 +15,23 @@ app.use(bodyParser.urlencoded());
 
 var router = express.Router();
 
-//@RequestParam('title')
+//@RequestParams -> query, search, count
 router.route('/find').get(function (req, res) {
-    var title = req.param('title');
-    console.log(title);
+  var reqParams = {
+    query: req.param('query'),
+    search: req.param('search'),
+    count: req.param('count')
+  };
 
-    imdbParser.find(title)
-              .then((result) => {
-                    res.json(result);
-              })
-              .catch((result) => {
-                    res.json(result);
-              });
+  console.log(JSON.stringify(reqParams));
+
+  imdbParser.find(reqParams)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((result) => {
+      res.json(result);
+    });
 
 });
 
@@ -35,5 +40,5 @@ app.use('/api', router); //This is our route middleware
 app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + server.address().port);
 });
